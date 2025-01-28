debb5629bd156031a26139843fc35805540d308d
gomminjae <gommj0611@naver.com>
2025-01-28 19:16:14 +0900

    Feat: post

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
@@ -7,17 +8,27 @@ export default function Header() {
         <h1 className="text-xl sm:text-2xl font-bold">
           <Link href="/">Gomminjae</Link>
         </h1>
-        <nav className="space-x-4">
+        <nav className="flex items-center space-x-6">
           <Link href="/" className="text-lg font-bold hover:text-blue-400">
             Home
           </Link>
           <Link href="/blog" className="text-lg font-bold hover:text-blue-400">
-            Blog
+            Posts
           </Link>
           <Link href="/about" className="text-lg font-bold hover:text-blue-400">
             About
           </Link>
-
+          {/* <Link 
+            href="https://github.com/gomminjae" 
+            className="flex items-center"
+          >
+            <Image
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              alt="GitHub"
+              width={36}
+              height={36}
+            />
+          </Link> */}
         </nav>
       </div>
     </header>
